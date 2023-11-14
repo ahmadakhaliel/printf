fb22492dab2a55c875fba5565ac5df0a6ddc3e34
AhmedAbdallah69 <aljwkrl@gmail.com>
2023-11-14 20:37:34 +0200

    printf

--- _printf.c
+++ _printf.c
@@ -8,49 +8,34 @@
  */
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int count = 0, x, len = 0;
-	char c, *str;
+	va_list args;int count = 0, x, len = 0;char c, *str;
+	
 	va_start(args, format);
 
 	while (*format != '\0')
 	{
 		if (*format != '%')
 		{
-			putchar(*format);
-			count++;
+			putchar(*format);count++;
 		}
 		else
 		{
 			switch (*++format)
 			{
 				case 'd':
-					x = va_arg(args, int);
-					write(1, &x, 1);
-					count++;
-					break;
+					x = va_arg(args, int);write(1, &x, 1);count++;break;
 				case 'c':
-					c = va_arg(args, char);
-					write(1, &c, 1);
-					count++;
-					break;
+					c = va_arg(args, char);write(1, &c, 1);count++;break;
 				case 's':
 					str = va_arg(args, char *);
 					while (str[len] != '\0')
 						len++;
-					write(1, str, len);
-					count += len;
-					break;
+					write(1, str, len);count += len;break;
 				default:
-					putchar('%');
-					putchar(*format);
-					count += 2;
-					break;
+					putchar('%');putchar(*format);count += 2;break;
 			}
 		}
 		format++;
 	}
-
-	va_end(args);
-	return (count);
+	va_end(args);return (count);
 }

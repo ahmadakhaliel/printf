2f601381cd09a5b9a33ed346a10535a089bb72db
AhmedAbdallah69 <aljwkrl@gmail.com>
2023-11-16 20:38:58 +0200

    k

--- _printf.c
+++ _printf.c
@@ -7,55 +7,60 @@
  */
 int _printf(const char *format, ...)
 {
-    int count;
-    va_list args;
+	int count;
+	va_list args;
 
-    va_start(args, format);
-    if (format == NULL)
-        return (-1);
+	va_start(args, format);
+	if (format == NULL)
+		return (-1);
 
-    count = useFun(format, args);
-    va_end(args);
-    return (count);
+	count = useFun(format, args);
+	va_end(args);
+	return (count);
 }
 
 int useFun(const char *format, va_list args)
 {
-    int i, j, count, checker;
+	int i, j, count, checker;
 
-    fmtsSpecifier f_list[] = {{"c", print_char}, {"d", print_int}, {"i", print_int}, {"s", printS},
+	fmtsSpecifier f_list[] = {{"c", print_char}, {"d", print_int}, {"i", print_int}, {"s", printS},
                             {"%", print_percent}, {NULL, NULL}};
 
-    count = 0;
+	count = 0;
 
-    for (i = 0; format[i] != '\0'; ++i) {
-
-        if (format[i] == '%'){
-            for (j = 0; f_list[j].sym != NULL; ++j) {
-                if (format[i + 1] == f_list[j].sym[0]){
-                    checker = f_list[j].f(args);
-                    if (checker == -1)
-                        return (-1);
-                    count += checker;
-                    break;
-                }
-            }
-            if (f_list[j].sym == NULL && format[i + 1] != ' '){
-                if (format[i + 1] != '\0') {
-                    _putchar(format[i]);
-                    _putchar(format[i + 1]);
-                    count += 2;
-                }
-                else
-                    return (-1);
-            }
-            i += 1;
-        }
-        else
-        {
-            _putchar(format[i]);
-            count++;
-        }
-    }
-    return (count);
+	for (i = 0; format[i] != '\0'; ++i)
+	{
+		if (format[i] == '%')
+		{
+			for (j = 0; f_list[j].sym != NULL; ++j)
+			{
+				if (format[i + 1] == f_list[j].sym[0])
+				{
+					checker = f_list[j].f(args);
+				if (checker == -1)
+						return (-1);
+					count += checker;
+					break;
+				}
+			}
+			if (f_list[j].sym == NULL && format[i + 1] != ' ')
+			{
+				if (format[i + 1] != '\0')
+				{
+					_putchar(format[i]);
+					_putchar(format[i + 1]);
+					count += 2;
+				}
+				else
+					return (-1);
+			}
+			i += 1;
+		}
+		else
+		{
+			_putchar(format[i]);
+			count++;
+		}
+	}
+	return (count);
 }
--- print_int.c
+++ print_int.c
@@ -6,22 +6,25 @@
  */
 int print_int(va_list args)
 {
-    char buffer[20];
-    int i = 0, x;
+	char buffer[20];
+	int i = 0, x;
 
-    int n = va_arg(args, int);
-    if (n < 0) {
-        _putchar('-');
-        n *= -1;
-    }
+	int n = va_arg(args, int);
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
 
-    while (n > 0) {
-        buffer[i++] = n % 10 + '0';
-        n /= 10;
-    }
-    x = i;
-    while (i > 0) {
-        _putchar(buffer[--i]);
-    }
-    return (x);
+	while (n > 0)
+	{
+		buffer[i++] = n % 10 + '0';
+		n /= 10;
+	}
+	x = i;
+	while (i > 0)
+		_putchar(buffer[--i]);
+
+	return (x);
 }
+

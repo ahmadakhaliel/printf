4f4b1aa3cfb0a76ae567eafe087709780b876c11
AhmedAbdallah69 <aljwkrl@gmail.com>
2023-11-16 16:47:31 +0200

    Ahmed Abdallah's take

--- _printf.c
+++ _printf.c
@@ -7,45 +7,55 @@
  */
 int _printf(const char *format, ...)
 {
+    int count;
     va_list args;
-    va_start(args, format);
 
-    int count = 0;
-    while (*format != '\0')
-    {
-        if (*format != '%')
-        {
-            _putchar(*format);
-            count++;
+    va_start(args, format);
+    if (format == NULL)
+        return (-1);
+
+    count = useFun(format, args);
+    va_end(args);
+    return (count);
+}
+
+int useFun(const char *format, va_list args)
+{
+    int i, j, count, checker;
+
+    fmtsSpecifier f_list[] = {{"c", print_char}, {"d", print_int}, {"i", print_int}, {"s", printS},
+                            {"%", print_percent}, {NULL, NULL}};
+
+    count = 0;
+
+    for (i = 0; format[i] != '\0'; ++i) {
+
+        if (format[i] == '%'){
+            for (j = 0; f_list[j].sym != NULL; ++j) {
+                if (format[i + 1] == f_list[j].sym[0]){
+                    checker = f_list[j].f(args);
+                    if (checker == -1)
+                        return (-1);
+                    count += checker;
+                    break;
+                }
+            }
+            if (f_list[j].sym == NULL && format[i + 1] != ' '){
+                if (format[i + 1] != '\0') {
+                    _putchar(format[i]);
+                    _putchar(format[i + 1]);
+                    count += 2;
+                }
+                else
+                    return (-1);
+            }
+            i += 1;
         }
         else
         {
-            // Handle format specifiers
-            switch (*++format)
-            {
-            case 'd': case 'i':
-                    count += print_int(args);
-                    break;
-                case 'c':
-                    count += print_char(args);
-                    break;
-                case 's':
-                    count += printS(args);
-                    break;
-                case '%':
-                    count += print_percent(args);
-                    break;
-                default:
-                    /* Handle unknown format specifiers */
-                    _putchar('%');
-                    _putchar(*format);
-                    count += 2;
-                    break;
-            }
+            _putchar(format[i]);
+            count++;
         }
-        format++;
     }
-
-    va_end(args);
-    return count;
+    return (count);
 }
--- main.h
+++ main.h
@@ -1,10 +1,21 @@
 #ifndef PRINTF_MAIN_H
 #define PRINTF_MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
 #include <stdarg.h>
+
+typedef struct formats
+{
+    char *sym;
+    int (*f)(va_list);
+} fmtsSpecifier;
+
 int _putchar(char c);
 int print_char(va_list list);
 int printS(va_list args);
 int print_percent(__attribute__((unused))va_list list);
 int print_int(va_list args);
+int useFun(const char *format, va_list args);
 int _printf(const char *format, ...);
+
 #endif
